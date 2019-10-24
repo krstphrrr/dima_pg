4022b43ea9f6a4628564fea35b8a7d18462e757f
Kris! <42949874+krstphrrr@users.noreply.github.com>
2019-10-24 16:49:57 -0600

    solved the multiple positional arguments issue

--- moretest.py
+++ moretest.py
@@ -1,4 +1,63 @@
 
+class A:
+    def ping(self):
+        print('ping:', self)
+
+class B(A):
+    def pong(self):
+        print('pong:', self)
+
+class C(A):
+    def pong(self):
+        print('PONG:', self)
+
+class D(B, C):
+    def ping(self):
+        super().ping() # A
+        print('post-ping:', self)
+    def pingpong(self):
+        self.ping() # D (calling A:A is superclass)
+        super().ping() # A
+        self.pong() # B
+        super().pong() # B
+        C.pong(self) # C explicit call to specific superclass
+
+a=A()
+a.ping()
+a
+
+
+
+
+b = B()
+b.pong()
+b.ping()
+
+c = C()
+c.pong()
+c.ping()
+
+d = D()
+
+d.pong()
+C.pong(d)
+A.ping(d)
+d.ping()
+
+
+d.pingpong()
+tkinter.Text.__mro__
+def print_mro(cls):
+    print(', '.join(c.__name__ for c in cls.__mro__))
+import tkinter
+print_mro(tkinter.Text)
+
+
+
+
+
+
+
 
 arcno = arcno()
 arcno.make('tblLines','C:\\Users\\kbonefont.JER-PC-CLIMATE4\\Desktop\\Some_data\\NM_TaosFO_LUP_2018_5-3b_01.mdb')
@@ -20,7 +79,51 @@ def name(arg1,arg2):
     return joined
 
 df = arcno.temp.copy(deep=True)
-import pickle
+index = df['LineKey']=='1808050843012839'
+df[~index]
+import operator
+df[operator.invert(index)]
+df
+basicfilter=basicfilter()
+basicfilter.select(df,'LineKey','1808050843012839')
+basicfilter.tempdf
+class basicfilter():
+    tempdf = None
+
+    def __init__(self, indf=None,field=None,*values):
+        import os, pandas as pd
+        self.indf = indf
+        self.field = field
+        self.values = values
+        self.index = None
+
+    def select(self,indf,field,*values):
+        import os, pandas as pd
+        self.indf = indf
+        self.field = field
+        self.values = values
+        self.index = None
+        dfset = []
+        def name(arg1,arg2):
+
+            self.arg1 = arg1
+            self.arg2 = arg2
+
+            joined= os.path.join(self.arg1+self.arg2)
+            return joined
+
+        for val in self.values:
+            self.index = self.indf[f'{self.field}'] == f'{val}'
+            exec("%s = self.indf[self.index]" % name(f'{self.field}',f'{val}'))
+            dfset.append(eval(name(f'{self.field}',f'{val}')))
+            self.tempdf = pd.concat(dfset)
+
+    def __invert__(self):
+        import operator
+        return operator.invert(self.index)
+
+
+
 
 pickle.dump(dflk)
 dflk = df['LineKey']
@@ -115,10 +218,11 @@ class arcno():
     def select(self, in_df,*vals, field = None):
         """
         right now:
-        - refactoring nested if statements
+        - refactoring nested if statements _done
         later:
-        - receiving multiple arguments for values instead of two
+        - receiving multiple arguments for values instead of two _done
         - overloading operator instead of keyword argument
+        --- unary opera
         """
         import pandas as pd
         self.in_df = in_df
