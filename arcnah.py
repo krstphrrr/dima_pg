bc81d2816dd98261abe5f0da2f116b09fb37a02d
Kris! <42949874+krstphrrr@users.noreply.github.com>
2019-11-13 13:54:41 -0700

    new methods working a-ok

--- arcnah.py
+++ arcnah.py
@@ -2,79 +2,54 @@ import pandas as pd
 from os import listdir,getcwd, chdir
 from os.path import normpath, join
 from methods.make_table import Table
-# replacing ap's gdb methods with pandas alternatives
-# if arcpy creates a temporary view in gdb, arcno creates dataframe within its temp class attribute
-##
-# if arcpy selects certain columns from temp view, arcno selects certain columns from df and returns them
-# if arcpy counts rows of temp view filtered with select, arcno counts rows of dataframe filtered through if statement dependent on a methods argument
+from utils import Acc
 
-# iris = pd.read_csv(r'C:\Users\kbonefont.JER-PC-CLIMATE4\Desktop\iris.csv')
-#
-# set(pd.Series(iris.columns))
-#
-# class df_sets:
-#     _df = None
-#     _cols = None
-#     def __init__(self,input):
-#         self.input = input
-#         self._cols = set(pd.Series(input.columns))
-#
+"""
+ replacing ap's gdb methods with pandas alternatives
+
+ - if ap creates a temporary view in gdb, arcno creates dataframe within its
+   temp class attribute
+
+ - if ap selects certain columns from temp view, arcno selects certain columns
+   from df and returns them
+
+ - if ap counts rows of temp view filtered with select, arcno counts rows of
+   dataframe filtered through if statement dependent on a methods argument
+
+"""
 
 
-#
-# param = msconfig()
-# con = pyodbc.connect(**param)
-# cur = con.cursor()
-# full_tbl_list = []
-# for tbl in cur.tables():
-#     if "tbl" in tbl[2]:
-#         cur1 = con.cursor()
-#         if tbl.table_type == "TABLE":
-#             full_tbl_list += [tbl.table_name,]
-#
-# path = normpath(r"C:\Users\kbonefont\Desktop\Some_data\NM_TaosFO_LUP_2018_5-3b_01.mdb")
-#
-#
-# arcno.MakeTableView_management('tblLines',path)
-# arcno.temp
-# arcno.SelectLayerByAttribute_management(arcno.temp,'1808051039437030','1808050843012839',field='LineKey')
-# arcno.temp_table
 
+class arcno():
+    tablelist = []
+    isolates = None
 
-class arcNo():
-    temp = None
-    string = None
-    uniq = None
-    frames = None
-    single_field = None
-    exist=None
-    temp_join=None
-    how = None
-    temp2 = None
-    temp_table = None
-
-    t1 = None
-    t2 = None
-    # in_table = None
-    def __init__(self, in_table = None, in_df = None, right_on=None, left_on = None, whichdima = None):
-        self.in_table = in_table
-        self.in_df = in_df
-        self.right_on = right_on
-        self.left_on = left_on
+    def __init__(self, whichdima = None):
+        """ Initializes a list of tables in dima accessible on tablelist.
+        ex.
+        arc = arcno(path_to_dima)
+        arc.tablelist
+        """
         self.whichdima = whichdima
-    def __call__(self):
-        return arcno.__call__()
+        if self.whichdima is not None:
+            cursor = Acc(self.whichdima).con.cursor()
+            for t in cursor.tables():
+                if t.table_name.startswith('tbl'):
+                    self.tablelist.append(t.table_name)
 
 
     def MakeTableView_management(self,in_table,whichdima):
-        """ connects to ms access mdb, selects a table
-        and copies it to a df
+        """ connects to Microsoft Access .mdb file, selects a table
+        and copies it to a dataframe.
+        ex.
+        arc = arcno()
+        arc.MakeTableView_management('table_name', 'dima_path')
         """
         self.in_table = in_table
         self.whichdima = whichdima
 
         try:
-            self.temp = Table(self.in_table, self.whichdima).temp
+            return Table(self.in_table, self.whichdima).temp
         except Exception as e:
             print(e)
 
@@ -101,23 +76,28 @@ class arcNo():
                     exec("%s = self.in_df[index]" % name(f'{self.field}',f'{val}'))
                     dfset.append(eval(name(f'{self.field}',f'{val}')))
 
-                self.temp_table = pd.concat(dfset)
+                return pd.concat(dfset)
             except Exception as e:
                 print(e)
         else:
             print("error")
     def GetCount_management(self,in_df):
+        """ Returns number of rows in dataframe
+        """
         self.in_df = in_df
         return self.in_df.shape[0]
 
     def AddJoin_management(self,
     in_df,df2,right_on=None,left_on=None):
         """ inner join on two dataframes on 1 or 2 fields
-
+        ex.
+        arc = arcno()
+        arc.AddJoin_management('dataframe_x', 'dataframe_y', 'field_a')
         """
-        import pandas as pd
         # self.temp_table = None
         d={}
+        self.right_on = None
+        self.left_on = None
 
         d[self.right_on] = right_on
         d[self.left_on] = left_on
@@ -128,23 +108,44 @@ class arcNo():
         if self.right_on==self.left_on and len(self.in_df.columns)==len(self.df2.columns):
             try:
                 frames = [self.in_df, self.df2]
-                self.temp_table = pd.concat(frames)
+                return pd.concat(frames)
             except Exception as e:
                 print(e)
                 print('1. field or fields invalid' )
         elif self.right_on==self.left_on and len(self.in_df.columns)!=len(self.df2.columns):
             try:
                 # frames = [self.in_df, self.df2]
-                self.temp_table = self.in_df.merge(self.df2, on = d[self.right_on], how='inner')
+                return self.in_df.merge(self.df2, on = d[self.right_on], how='inner')
             except Exception as e:
                 print(e)
                 print('2. field or fields invalid')
         else:
             try:
-                self.temp_table=self.in_df.merge(self.df2,right_on=d[self.right_on], left_on=d[self.left_on])
+                return self.in_df.merge(self.df2,right_on=d[self.right_on], left_on=d[self.left_on])
             except Exception as e:
                 print(e)
                 print('3. field or fields invalid')
+
+    def CalculateField_management(self,in_df,newfield,*fields):
+        """ Creates a newfield by concatenating any number of existing fields
+        ex.
+        arc = arcno()
+        arc.CalculateField_management('dataframe_x', 'name_of_new_field', 'field_x', 'field_y','field_z')
+
+        field_x = 'red'
+        field_y = 'blue'
+        field_z = 'green'
+
+        name_of_new_field = 'redbluegreen'
+        """
+        self.in_df = in_df
+        self.newfield = newfield
+        self.fields = fields
+
+        self.in_df[f'{self.newfield}'] = (self.in_df[[f'{field}' for field in self.fields]].astype(str)).sum(axis=1)
+        return self.in_df
+
+
 
     def AddField_management(self, in_df, newfield):
         """ adds empty field within 'in_df' with fieldname
@@ -154,6 +155,7 @@ class arcNo():
         self.newfield = newfield
 
         self.in_df[f'{self.newfield}'] = pd.Series()
+        return self.in_df
 
     def RemoveJoin_management(self):
         """ creates deep copy of original dataset
@@ -162,27 +164,23 @@ class arcNo():
         """
         pass
 
-    def MakeFeatureLayer_management(self):
-        """ may be similar to screate table view
+    def isolateFields(self,in_df,*fields):
+        """ creates a new dataframe with submitted
+        fields.
         """
-        pass
+        self.fields = fields
+        self.isolates =self.in_df[[f'{field}' for field in self.fields]]
+        return self.isolates
+
+
+    # def MakeFeatureLayer_management(self):
+    #     """ may be similar to screate table view
+    #     """
+    #     pass
 
     def GetParameterAsText(self,string):
         """ return stringified element
 
         """
-
         self.string = f'{string}'
         return self.string
-
-
-
-"""
-Starting DIMA - C:\\Users\\kbonefont.JER-PC-CLIMATE4\\Desktop\\Some_data\\db.mdb
-   Starting Dups check
-used query:SELECT * FROM "tblGapDetail"
-df detected
-starting try..
-field not found
-use SelectLayerByAttribute first
-      tblGapDetail - No Dups found"""
